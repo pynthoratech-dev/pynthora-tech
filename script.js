a871510dc20294d62ab8f3d3a5742fecb9b1ec33
pynthoratech-dev <pynthoratech@gmail.com>
2025-09-09 12:22:22 +0100

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// Simple script for testing
-console.log("Pynthora Tech website loaded successfully!");
+// Simple script for future interactivity
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Pynthora Tech website loaded successfully!");
+});
